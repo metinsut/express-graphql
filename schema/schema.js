d53e325269a10689d1c3025c56ac7793685fc9f1
metin_sut <metin.sut@beyn.com.tr>
2019-01-30 18:05:51 +0300

    first

---
+++ schema/schema.js
@@ -0,0 +1,35 @@
+const graphql = require("graphql");
+const axios = require("axios");
+const _ = require("lodash");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+   { id: "23", firstName: "John", age: 31 },
+   { id: "24", firstName: "Jeny", age: 25 }
+]
+
+const UserType = new GraphQLObjectType({
+   name: 'User',
+   fields: {
+      id: { type: GraphQLString },
+      firstName: { type: GraphQLString },
+      age: { type: GraphQLInt }
+   }
+});
+
+const RootQuery = new GraphQLObjectType({
+   name: 'RootQueryType',
+   fields: {
+      user: {
+         type: UserType,
+         args: { id: { type: GraphQLString } },
+         resolve(parentValue, args) {
+            return axios.get(`http://localhost:3000/users/${args.id}`).then(resp => resp.data)
+         }
+      }
+   }
+})
+
+module.exports = new GraphQLSchema({
+   query: RootQuery
+})
