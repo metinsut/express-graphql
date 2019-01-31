2ac9639721f10bee572b1b081d132408aca9f31d
metin_sut <metin.sut@beyn.com.tr>
2019-01-31 13:06:23 +0300

    nested fields

--- schema/schema.js
+++ schema/schema.js
@@ -1,20 +1,37 @@
 const graphql = require("graphql");
 const axios = require("axios");
 const _ = require("lodash");
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = graphql;
 
-const users = [
-   { id: "23", firstName: "John", age: 31 },
-   { id: "24", firstName: "Jeny", age: 25 }
-]
+
+const CompanyType = new GraphQLObjectType({
+   name: 'Company',
+   fields: () => ({
+      id: { type: GraphQLString },
+      name: { type: GraphQLString },
+      description: { type: GraphQLString },
+      users: {
+         type: new GraphQLList(UserType),
+         resolve(parentValue, args) {
+            return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`).then(resp => resp.data)
+         }
+      }
+   })
+})
 
 const UserType = new GraphQLObjectType({
    name: 'User',
-   fields: {
+   fields: () => ({
       id: { type: GraphQLString },
       firstName: { type: GraphQLString },
-      age: { type: GraphQLInt }
-   }
+      age: { type: GraphQLInt },
+      company: {
+         type: CompanyType,
+         resolve(parentValue, args) {
+            return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`).then(resp => resp.data)
+         }
+      }
+   })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -25,6 +42,13 @@ const RootQuery = new GraphQLObjectType({
          args: { id: { type: GraphQLString } },
          resolve(parentValue, args) {
             return axios.get(`http://localhost:3000/users/${args.id}`).then(resp => resp.data)
+         }
+      },
+      company: {
+         type: CompanyType,
+         args: { id: { type: GraphQLString } },
+         resolve(parentValue, args) {
+            return axios.get(`http://localhost:3000/companies/${args.id}`).then(resp => resp.data)
          }
       }
    }
